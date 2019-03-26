8c112a3c823920564d2947c660d509f280f6647d
yazeedhathal <abohathal1@gmail.com>
2019-03-26 18:09:27 +0300

    06 lab

--- starter-code/monkey.js
+++ starter-code/monkey.js
@@ -15,3 +15,34 @@ Exercise your monkeys by retrieving their properties and using their methods. Pr
 for retrieving properties (dot notation and brackets).
 
 */
+
+// var Monkey = {}
+
+// Monkey.name = "ram";
+
+// Monkey.species ="yard";
+
+// Monkey.foodsEaten = ["banana", "apple"];
+
+// console.log(Monkey);
+
+var Monkey = function (name, species){
+
+this.name = name;
+this.species = species;
+this.foodsEaten = [];
+
+}
+
+Monkey.prototype.eatSomething = function(food
+){
+    
+   this.foodsEaten.push(food);
+}
+
+Monkey.prototype.introduce = function(){
+
+	    console.log(this.name + ' is ' + this.species +'i love to eat'+  this.foodsEaten );
+
+
+}
